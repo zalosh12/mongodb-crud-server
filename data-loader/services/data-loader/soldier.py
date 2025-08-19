dc847379fcc337d8746b3a93804acdd096fd13c9
Tzvi Zait <z0548430441@gmail.com>
2025-08-19 17:24:49 +0300

    create a class soldier

---
+++ data-loader/services/data-loader/soldier.py
@@ -0,0 +1,9 @@
+
+
+class Soldier:
+    def __init__(self,ID, first_name, last_name, phone_number, rank):
+        self.ID = ID
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.rank = rank
